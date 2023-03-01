259f70e18e67bf8bee0b6adbafcda86675511318
Jwarren619 <jjeremiahwarren@yahoo.com>
2023-02-28 23:49:52 -0600

    followed through with the tasks for the logiform.js and made sure to replace the loginUser functionality from the API with what was ther at first..the LOGIN_USER

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -26,14 +27,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      console.log(userFormData)
+      const { data } = await login( {
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
+      console.log(data)
+      Auth.login(data.login.token);
       Auth.login(token);
     } catch (err) {
       console.error(err);
